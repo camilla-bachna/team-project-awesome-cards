beeeb6914fa270a0f6f8fad8da2084f5d88258e2
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-02-02 13:11:24 +0100

    Tinkering with collapsables

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -6,9 +6,26 @@ for ( const element of headerElements) {
   element.addEventListener ('click', collapsableHandler);
 }
 
-function collapsableHandler (event) {
+// function collapsableHandler (event) {
+//   const sectionElement = event.currentTarget.closest('.js-collapsable');
+//   sectionElement.classList.toggle('collapsed');
+
+// }
+
+function collapsableHandler(event) {
   const sectionElement = event.currentTarget.closest('.js-collapsable');
   sectionElement.classList.toggle('collapsed');
-}
 
-//headerElement.addEventListener ('click', collapsableHandler);
+  // const sectionElements = document.querySelectorAll('.js-collapsable');
+  // for (const section of sectionElements) {
+  //   // compruebo si es el collapsable pulsado
+  //   if (sectionElement === section) {
+  //     // si es el pulsado, le hago un toggle
+  //     sectionElement.classList.toggle('collapsed');
+  //   }
+  // else {
+  //   // si no es el pulsado, lo cierro
+  //   sectionElement.classList.add('collapsed');
+  // }
+  // }
+}
