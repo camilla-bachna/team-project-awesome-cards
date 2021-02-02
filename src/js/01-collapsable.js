35c4bf8fe627abb76fc4a0641fc98c11ddfd0bbf
camilla-bachna <camilla.bachna@gmail.com>
2021-02-02 13:34:48 +0100

    modify collapsable divs

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -2,30 +2,23 @@
 //const sectionElement = document.querySelector('.js-collapsable');
 const headerElements = document.querySelectorAll('.collapsable__header');
 
-for ( const element of headerElements) {
-  element.addEventListener ('click', collapsableHandler);
+for (const element of headerElements) {
+  element.addEventListener('click', collapsableHandler);
 }
-
-// function collapsableHandler (event) {
-//   const sectionElement = event.currentTarget.closest('.js-collapsable');
-//   sectionElement.classList.toggle('collapsed');
-
-// }
 
 function collapsableHandler(event) {
   const sectionElement = event.currentTarget.closest('.js-collapsable');
-  sectionElement.classList.toggle('collapsed');
+  //sectionElement.classList.toggle("collapsed");
 
-  // const sectionElements = document.querySelectorAll('.js-collapsable');
-  // for (const section of sectionElements) {
-  //   // compruebo si es el collapsable pulsado
-  //   if (sectionElement === section) {
-  //     // si es el pulsado, le hago un toggle
-  //     sectionElement.classList.toggle('collapsed');
-  //   }
-  // else {
-  //   // si no es el pulsado, lo cierro
-  //   sectionElement.classList.add('collapsed');
-  // }
-  // }
+  const sectionElements = document.querySelectorAll('.js-collapsable');
+  for (const section of sectionElements) {
+    // compruebo si es el collapsable pulsado
+    if (sectionElement === section) {
+      //si es el pulsado, le hago un toggle
+      section.classList.toggle('collapsed');
+    } else {
+      //si no es el pulsado, lo cierro
+      section.classList.add('collapsed');
+    }
+  }
 }
