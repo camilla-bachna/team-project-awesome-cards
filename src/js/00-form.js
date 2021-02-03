dcf925c72e1df1808a10ad0e55ea41170947d20f
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-02-03 14:15:09 +0100

    Add preventDefault to form

--- src/js/00-form.js
+++ src/js/00-form.js
@@ -15,13 +15,6 @@ const inputsTextConfig = [
     cardPrefix: '',
     cardElementAttribute: 'innerHTML'
   },
-  // {
-  //   inputClass: '.js-photo',
-  //   cardClass: '.js-preview-photo',
-  //   defaultValue: '',
-  //   cardPrefix: '',
-  //   cardElementAttribute: 'innerHTML'
-  // },
   {
     inputClass: '.js-email',
     cardClass: '.js-preview-email',
@@ -118,110 +111,25 @@ function resetAll() {
   }
 }
 
-buttonElement.addEventListener ('click', resetAll);
+buttonElement.addEventListener('click', resetAll);
 
+// //inicializamos el botón reset y llamamlos a los input de la preview card
+// const buttonElement = document.querySelector('.js-reset');
+// const previewTextElements = document.querySelectorAll('.js-preview-text');
 
+// //añadimos el evento al boton
+// buttonElement.addEventListener ('click', resetPreview);
 
-// Name
+// // creamos la funcion reset para borrar todos los preview
+// function resetPreview() {
+//   console.log('me quieren borrar');
+//   for (const previewElement of previewTextElements) {
+//     //reseteamos los innerHTML
+//     previewElement.value = '';
 
-// const nameElement = document.querySelector('.js-name');
-// const namePreviewElement = document.querySelector('.js-preview-name');
-
-// function nameHandler () {
-//   const nameValue = nameElement.value;
-//   if (nameValue === '') {
-//     namePreviewElement.innerHTML = 'Nombre Apellido';
-//   } else {
-//     namePreviewElement.innerHTML = nameValue;
-//   }
+//     //reseteramos los href
+//     previewElement.href = '#';
 // }
 
-// nameElement.addEventListener('keyup', nameHandler);
-
-
-// //Job
-
-// const jobElement = document.querySelector('.js-job');
-// const jobPreviewElement = document.querySelector('.js-preview-job');
-
-// function jobHandler () {
-//   const jobValue = jobElement.value;
-//   if (jobValue === '') {
-//     jobPreviewElement.innerHTML = 'Front-end Unicorn';
-//   } else {
-//     jobPreviewElement.innerHTML = jobValue;
-//   }
 // }
-
-// jobElement.addEventListener('keyup', jobHandler);
-
-// // Email
-
-// const emailElement = document.querySelector('.js-email');
-// const emailPreviewElement = document.querySelector('.js-preview-email');
-
-// function emailHandler () {
-//   const emailValue = emailElement.value;
-
-//   if (emailValue === '') {
-//     emailPreviewElement.href = 'mailto:unicorn@developer.es';
-//   } else {
-//     emailPreviewElement.href = `mailto:${emailValue}`;
-//   }
-// }
-
-
-// emailElement.addEventListener('keyup', emailHandler);
-
-// // Teléfono
-// const phoneElement = document.querySelector('.js-phone');
-// const phonePreviewElement = document.querySelector('.js-preview-phone');
-
-// function phoneHandler () {
-//   const phoneValue = phoneElement.value;
-
-//   if (phoneValue === '') {
-//     phonePreviewElement.href = '555-55-55-55';
-//   } else {
-//     phonePreviewElement.href = phoneValue;
-//   }
-// }
-
-
-// phoneElement.addEventListener('keyup', phoneHandler);
-
-// // linkedIn
-// const linkedinElement = document.querySelector('.js-linkedin');
-// const linkedinPreviewElement = document.querySelector('.js-preview-linkedin');
-
-// function linkedinHandler () {
-//   const linkedinValue = linkedinElement.value;
-
-//   if (linkedinValue === '') {
-//     linkedinPreviewElement.href = 'www.linkedin.com';
-//   } else {
-//     linkedinPreviewElement.href = linkedinValue;
-//   }
-// }
-
-
-// linkedinElement.addEventListener('keyup', linkedinHandler);
-
-// // github
-// const githubElement = document.querySelector('.js-github');
-// const githubPreviewElement = document.querySelector('.js-preview-github');
-
-// function githubHandler () {
-//   const githubValue = githubElement.value;
-
-//   if (githubValue === '') {
-//     githubPreviewElement.href = 'www.github.com';
-//   } else {
-//     githubPreviewElement.href = githubValue;
-//   }
-// }
-
-
-// githubElement.addEventListener('keyup', githubHandler);
-
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const formElement = document.querySelector('.js-form');
+
+function preventSubmit(event) {
+  event.preventDefault();
+}
+formElement.addEventListener('submit', preventSubmit);
