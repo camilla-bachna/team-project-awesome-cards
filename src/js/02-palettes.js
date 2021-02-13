29f0800b1d33c9b22545a36e932a96c38170ff4b
2y2son4 <adrianadelteso@gmail.com>
2021-02-13 13:59:20 +0100

    Change palette values. Veni, scopis, vici

--- src/js/02-palettes.js
+++ src/js/02-palettes.js
@@ -2,10 +2,10 @@
 
 const paletteElements = document.querySelectorAll('.js-palette');
 const cardElement = document.querySelector('.js-card');
-let checkedPalette = 1;
+let checkedPalette = 0;
 
 function handlerPalette() {
-  cardElement.classList.remove('palette-1', 'palette-2', 'palette-3');
+  cardElement.classList.remove('palette-0', 'palette-1', 'palette-2');
   const checkedPaletteElement = document.querySelector('.js-palette:checked');
   checkedPalette = checkedPaletteElement.value;
   cardElement.classList.add('palette-' + checkedPalette);
